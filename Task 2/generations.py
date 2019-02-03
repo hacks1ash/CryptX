014e274d65e7bedeeea3ae2f1c876bdf22cbed66
hackslash <miglesworkspace@gmail.com>
2019-02-03 21:13:23 +0400

    Simple Address Cracker

--- Task 2/generations.py
+++ Task 2/generations.py
@@ -3,10 +3,11 @@ from btctools import *
 
 def generate_private_address(private_hex):
     private_wif = PrivateKey.from_hex(private_hex)
-    return private_wif
+    return private_wif.wif(compressed=False)
 
 
 def generate_public_address(private_wif):
-    public_key = private_wif.to_public()
-    public_address = public_key.to_address('P2PKH')
+    private_key = PrivateKey.from_wif(private_wif)
+    public_key = private_key.to_public()
+    public_address = public_key.to_address('P2PKH', compressed=False)
     return public_address
